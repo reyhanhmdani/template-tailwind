a8e5f7ff6c81d76c6fd67904b0582e972ed53df8
reyhanhmdani <rey7dan7@gmail.com>
2024-12-09 13:58:42 +0700

    final (tema sesuai tema windows)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['index.html'],
-    theme: {
-        container: {
-            center: true,
-            padding: '16px'
-        },
-        extend: {
-            colors: {
-                Primary: '#ED2939',
-                dark: '#0f172a',
-                abu_abu: '#64748b',
-            },
-            screens: {
-                'xl': '1024px',
-            }
-        },
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
     },
-    plugins: [],
-}
-
+    extend: {
+      colors: {
+        Primary: "#ED2939",
+        dark: "#0f172a",
+        abu_abu: "#64748b",
+      },
+      screens: {
+        xl: "1024px",
+      },
+    },
+  },
+  plugins: [],
+};
